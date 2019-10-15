bbb8de375c2542a36a805150df648a3eede6a391
Wayland FTC <ftc@Waylandunion.org>
2019-10-15 18:08:29 -0400

    i added two more classes and completed the slidebothardware and slidebotrobot. -Dominik

---
+++ SlideBotHardware.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class SlideBotHardware {
+
+    public DcMotor leftDrive = null;
+    public DcMotor rightDrive = null;
+    public DcMotor leftrearDrive = null;
+    public DcMotor rightrearDrive = null;
+
+    HardwareMap hwMap = null;
+
+    public SlideBotHardware() {
+
+    }
+
+    public void init (HardwareMap ahwMap) {
+
+        hwMap = ahwMap;
+        leftDrive = hwMap.get(DcMotor.class, "lfd");
+        rightDrive = hwMap.get(DcMotor.class, "rfd");
+        leftrearDrive = hwMap.get(DcMotor.class, "lrd");
+        rightrearDrive = hwMap.get(DcMotor.class, "rrd");
+
+        leftDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftrearDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightrearDrive.setDirection(DcMotor.Direction.REVERSE);
+
+        leftDrive.setPower(0);
+        rightDrive.setPower(0);
+        leftrearDrive.setPower(0);
+        rightrearDrive.setPower(0);
+
+        leftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftrearDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightrearDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+
+    }
+
+}
---
+++ SlideBotRobot.java
@@ -0,0 +1,55 @@
+  package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.teamcode.SlideBotHardware;
+
+public class SlideBotRobot {
+    SlideBotHardware myself = new SlideBotHardware();
+    boolean moving;
+
+    public void  initrobot(){
+        moving=false;
+    }
+    public void drive (double lpower, double rpower){
+        myself.rightDrive.setPower(rpower);
+        myself.leftDrive.setPower(lpower);
+        moving=true;
+    }
+
+    public void initHW(HardwareMap ahwMap) {
+        myself.init(ahwMap);
+
+    }
+    public void DriveReverse(double power) {
+        drive(-power, -power);
+    }
+    public void DriveForward(double power) {
+        drive(power, power);
+    }
+    public void DriveLeft(double power) {
+        drive(-power, power);
+    }
+    public void DriveRight(double power) {
+        drive(power, -power);
+    }
+
+    public void DriveStop() {
+        myself.leftDrive.setPower(0);
+        myself.rightDrive.setPower(0);
+        moving = false;
+    }
+
+    public boolean ISMoving() {
+        return moving;
+    }
+
+    public void RightDrive(double power){
+        myself.rightDrive.setPower(power);
+        myself.rightrearDrive.setPower(power);
+    }
+
+    public void LeftDrive(double power){
+        myself.leftDrive.setPower(power);
+        myself.leftrearDrive.setPower(power);
+    }
+}
